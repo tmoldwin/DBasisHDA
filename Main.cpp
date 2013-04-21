5796bf9140be3ae39d01a678e7a50b6e397203c7
Joshua A Blumenkopf <joshblumenkopf@dyn-152-100.wilf.wireless.yu.edu>
2013-04-21 04:43:45 -0400

    Revert "Revert "Revert "broke implication into header and main file, started testing table in main"""
    This reverts commit a2c94f99265c15ecd4ed85f6b06f6c7bd2b281c6.

--- Main.cpp
+++ Main.cpp
@@ -56,14 +56,7 @@ void testReadFile() {
     std::vector<std::vector<char> > * matrix = readTable("table1.txt");
     printMatrix(matrix);
 }
-void testReduceTable(){
-    std::vector<std::vector<char> >  matrix = *readTable("tablereducetest.txt");
-    Table test(matrix);
-    std::vector<std::vector<char> >  matrix3=test.get_matrix();
-    printMatrix(&matrix3);
-}
 
-<<<<<<< HEAD
 int main(int argc, char **argv) {
     std::cout << "\nTesting readFile()\n";
     testReadFile();
@@ -73,9 +66,4 @@ int main(int argc, char **argv) {
     testComplement();
     std::cout << "\nTesting Intersection\n";
     testIntersection();
-=======
-int main(int argc, char **argv){
-   // testReadFile();
-    testReduceTable();
->>>>>>> broke implication into header and main file, started testing table in main
 }
